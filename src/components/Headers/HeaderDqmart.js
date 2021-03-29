efb6198e46a954044d2fa5c6283f0cbe1068ecc1
abdulaziz1999 <azizmentor96@gmail.com>
2021-03-29 14:45:51 +0700

    header dqmart

---
+++ src/components/Headers/HeaderDqmart.js
@@ -0,0 +1,69 @@
+import React from "react";
+
+// reactstrap components
+import { Card, CardBody, CardTitle, Container, Row, Col } from "reactstrap";
+
+const Header = () => {
+  return (
+    <>
+      <div className="header bg-gradient-info pb-8 pt-5 pt-md-8">
+        <Container fluid>
+          <div className="header-body">
+            {/* Card stats */}
+            <Row>
+              <Col lg="6" xl="6">
+                <Card className="card-stats mb-4 mb-xl-0">
+                  <CardBody>
+                    <Row>
+                      <div className="col">
+                        <CardTitle
+                          tag="h5"
+                          className="text-uppercase text-muted mb-0"
+                        >
+                          Jumlah Pegawai
+                        </CardTitle>
+                        <span className="h2 font-weight-bold mb-0">
+                          422
+                        </span>
+                      </div>
+                      <Col className="col-auto">
+                        <div className="icon icon-shape bg-danger text-white rounded-circle shadow">
+                          <i className="fas fa-chart-bar" />
+                        </div>
+                      </Col>
+                    </Row>
+                   
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col lg="6" xl="6">
+                <Card className="card-stats mb-4 mb-xl-0">
+                  <CardBody>
+                    <Row>
+                      <div className="col">
+                        <CardTitle
+                          tag="h5"
+                          className="text-uppercase text-muted mb-0"
+                        >
+                          Jumlah Kredit
+                        </CardTitle>
+                        <span className="h2 font-weight-bold mb-0">2,356</span>
+                      </div>
+                      <Col className="col-auto">
+                        <div className="icon icon-shape bg-warning text-white rounded-circle shadow">
+                          <i className="fas fa-chart-pie" />
+                        </div>
+                      </Col>
+                    </Row>
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
+          </div>
+        </Container>
+      </div>
+    </>
+  );
+};
+
+export default Header;
