1cabae84a9c5518baf1e7e52f3e47d176d36788d
abdulaziz1999 <azizmentor96@gmail.com>
2021-08-09 17:10:02 +0700

    Table tunjangan

---
+++ src/components/Table/TableMasterTunjangan.jsx
@@ -0,0 +1,82 @@
+// import axios from "axios";
+
+import React from "react";
+import { useState } from "react";
+import { Col, Form, FormGroup , Input,InputGroup, InputGroupAddon,InputGroupText, Row, Table,Button} from "reactstrap";
+
+
+const TableTunjangan = ({data,modal,remove}) => {
+    const [searchTerm, setSearchTerm] = useState("")
+    
+    return (
+        <div className="">
+            <Form>
+                <Row>
+                    <Col md="12">
+                        <FormGroup>
+                            <InputGroup className="input-group-alternative mb-4">
+                            <InputGroupAddon addonType="prepend">
+                                <InputGroupText>
+                                <i className="ni ni-zoom-split-in" />
+                                </InputGroupText>
+                            </InputGroupAddon>
+                            <Input className="form-control-alternative" placeholder="Search" type="text" onChange={(event) => {setSearchTerm(event.target.value) }}/>
+                            </InputGroup>
+                        </FormGroup>
+                    </Col>
+                </Row>
+            </Form>
+            <Table className="align-items-center table-flush" responsive>
+                  <thead className="thead-light">
+                    <tr >
+                      <th scope="col">Nama Tunjangan</th>
+                      <th scope="col">Nominal</th>
+                      <th scope="col">Type</th>
+                      <th scope="col" className="text-center">Act</th>
+                    </tr>
+                  </thead>
+                  <tbody >
+                    {data.filter((val) => {
+                        if(searchTerm === ""){
+                            return val
+                        }else if(val.tunjangan.toLowerCase().includes(searchTerm.toLowerCase())){
+                            return val
+                        }
+                        return ""
+                    }).map((post, index) => {
+                      return (
+                        <tr key={index} className="text-left">
+                          <td>{post.tunjangan}</td>
+                          <td>{post.nominal}</td>
+                          <td>{post.type_name}</td>
+                          <td>
+                            <div className="btn-group">
+                              <Button
+                                color="info"
+                                type="button"
+                                size="sm"
+                                onClick={() => modal("exampleModal", post)}
+                              >
+                                <i className="ni ni-ruler-pencil"></i> Update
+                              </Button>
+                              &nbsp;
+                              <Button
+                                color="danger"
+                                type="button"
+                                size="sm"
+                                onClick={() => remove(post.id)}
+                              >
+                                <i className="fa fa-trash"></i> Delete
+                              </Button>
+                            </div>
+                          </td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </Table>
+        </div>
+    )
+}
+
+export default TableTunjangan;
