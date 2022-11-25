e75a99b8ee921940411f758b96d511ccf84f3cc5
Abdul Aziz <36178522+abdulaziz1999@users.noreply.github.com>
2022-11-25 18:05:39 +0700

    Update Config.js

--- src/service/Config.js
+++ src/service/Config.js
@@ -1,2 +1,2 @@
 export const Rootpath = "https://kepegawaian.dqakses.id/api"
-export const RootOnline = "https://backendpayroll.dev.dqakses.id/api"
+export const RootOnline = "https://bepayroll.live.dqakses.id/api"
